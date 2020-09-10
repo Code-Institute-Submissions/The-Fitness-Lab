c869a376956d1428301fd7906074ee1a6f6664ad
john <jericcarreon@yahoo.com>
2020-09-10 01:45:43 +0100

    Added cart app, context.py

---
+++ cart/contexts.py
@@ -0,0 +1,30 @@
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
+
+def cart_session(request):
+    """
+    Context for the cart for the user in session
+    """
+    cart_items = []
+    total = 0
+    product_total = 0
+    cart = request.session.get('cart', {})
+
+    for id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=id)
+        total += quantity * product.price
+        product_total += quantity
+        cart_items.append({
+            'id': id,
+            'quantity': quantity,
+            'product': product,
+        })
+
+    context = {
+        'cart_items': cart_items,
+        'total': total,
+        'product_count': product_total,
+    }
+
+    return context
