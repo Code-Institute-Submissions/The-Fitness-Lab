8fb6c25c7111d3fb27d381ada293a734645d7c4d
john <jericcarreon@yahoo.com>
2020-09-10 01:48:00 +0100

    Added cart app views.py add to cart function

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+
+from products.models import Product
+
 
 # Create your views here.
 
@@ -8,3 +12,24 @@ def cart_contents(request):
     View that renders the cart content page
     """
     return render(request, "cart.html")
+
+
+def add_to_cart(request, id):
+    """
+    Add a quantity of the specified product to the shopping bag
+    """
+    product = get_object_or_404(Product, pk=id)
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+    if id in cart:
+        cart[id] = int(cart[id]) + quantity
+        messages.success(
+            request, f'Updated {product.name} quantity to {cart[id]}')
+    else:
+        cart[id] = cart.get(id, quantity)
+        messages.success(request, f'Added {product.name} to your bag')
+
+    request.session['cart'] = cart
+    print(request.session['cart'])
+    return redirect(redirect_url)
