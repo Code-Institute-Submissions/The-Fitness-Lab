060d8ba47d68668c0fdc9af56e4fbe170e93f071
john <jericcarreon@yahoo.com>
2020-09-11 01:58:33 +0100

    Added cart app urls.py adjust_cart and cart_remove

--- cart/urls.py
+++ cart/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.cart_contents, name='cart'),
     path('add/<id>/', views.add_to_cart, name='add_to_cart'),
+    path('adjust/<item_id>/', views.adjust_cart, name='adjust_cart'),
+    path('remove/<item_id>/', views.cart_remove, name='cart_remove')
 ]
