a6c68d7584d8764b3f53f526adf44dd92e33e117
john <jericcarreon@yahoo.com>
2020-09-17 19:34:31 +0100

    deleted checkout apps, admin fields country and town

--- checkout/admin.py
+++ checkout/admin.py
@@ -13,8 +13,8 @@ class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date', 'order_total',
                        'grand_total', 'original_cart', 'stripe_pid')
     fields = ('order_number', 'date', 'full_name',
-              'email', 'phone_number', 'country',
-              'postcode', 'town_or_city', 'street_address1',
+              'email', 'phone_number',
+              'postcode', 'street_address1',
               'street_address2', 'county',
               'order_total', 'grand_total', 'original_cart', 'stripe_pid')
 
