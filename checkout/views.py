30f84f8324d4b1809a92f46cf8d6a54cb7f29975
john <jericcarreon@yahoo.com>
2020-09-17 19:31:30 +0100

    Added checkout apps, views.py checkout and thanks

--- checkout/views.py
+++ checkout/views.py
@@ -9,25 +9,112 @@ from products.models import Product
 
 from cart.contexts import cart_session
 
-""" import stripe
- """
+import stripe
 import json
 # Create your views here.
 
 
+def thanks(request, order_number):
+    """
+    Renders successfull payments in checkout
+    """
+    save_info = request.session.get('save_info')
+    order = get_object_or_404(Order, order_number=order_number)
+
+    if request.user.is_authenticated:
+        profile = UserProfile.objects.get(user=request.user)
+        # Attach the user's profile to the order
+        order.user_profile = profile
+        order.save()
+
+        # Save the user's info
+        if save_info:
+            profile_data = {
+                'default_phone_number': order.phone_number,
+                'default_postcode': order.postcode,
+                'default_street_address1': order.street_address1,
+                'default_street_address2': order.street_address2,
+                'default_county': order.county,
+            }
+            user_profile_form = UserProfileForm(profile_data, instance=profile)
+            if user_profile_form.is_valid():
+                user_profile_form.save()
+
+    messages.success(request, f'Payment Succesful!')
+
+    if 'cart' in request.session:
+        del request.session['cart']
+
+    template = 'thanks.html'
+    context = {
+        'order': order,
+    }
+
+    return render(request, template, context)
+
+
 def checkout(request):
-    """renders checkout page"""
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    cart = request.session.get('cart', {})
-    if not cart:
-        messages.error(
-            request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+    if request.method == 'POST':
+        cart = request.session.get('cart', {})
+        form_data = {
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'postcode': request.POST['postcode'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'county': request.POST['county'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_cart = json.dumps(cart)
+            order.save()
+            for item_id, quantity in cart.items():
+                product = Product.objects.get(id=item_id)
+                if isinstance(quantity, int):
+                    order_line_item = OrderItem(
+                        order=order,
+                        product=product,
+                        quantity=quantity,
+                    )
+                    order_line_item.save()
 
+            request.session['save_info'] = 'save-info' in request.POST
+            return redirect(reverse('thanks', args=[order.order_number]))
+        else:
+            messages.error(
+                request, 'Unable to process your payment, Please check your information')
+    else:
+        cart = request.session.get('cart', {})
+        if not cart:
+            messages.error(
+                request, "The cart is empty")
+            return redirect(reverse('products'))
+
+        current_cart = cart_session(request)
+        total = current_cart['total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
     order_form = OrderForm()
     template = 'checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
