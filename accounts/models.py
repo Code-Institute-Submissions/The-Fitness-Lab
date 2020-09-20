d9f63d994f12a529a715a9a81cd1ebe7b0e228ed
john <jericcarreon@yahoo.com>
2020-09-20 20:22:06 +0100

    Added Accounts app models UserAccount and save_user

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+
+class UserAccount(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    email = models.CharField(max_length=200, null=True)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+    street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    county = models.CharField(max_length=80, null=True, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def save_user_account(sender, instance, created, **kwargs):
+    """
+# Create or update the user profile
+"""
+    if created:
+        UserAccount.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.useraccount.save()
