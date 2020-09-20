09fc9b504f6c6dc012817f01be631b72441970df
john <jericcarreon@yahoo.com>
2020-09-20 20:31:38 +0100

    Added account app forms.py Profile form

---
+++ accounts/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from .models import UserAccount
+
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserAccount
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'name',
+            'email': 'email',
+            'phone_number': 'Phone Number',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+            'county': 'County, State or Locality',
+        }
+
+        self.fields['name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field:
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = ('border-black '
+                                                        'rounded-0 '
+                                                        'profile-form-input')
+            self.fields[field].label = False
