e582094047b7a37af4fb83342793cf8a75fc2315
john <jericcarreon@yahoo.com>
2020-09-23 21:53:45 +0100

    Added Jquery for pills-tab and navbar scroll functions

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,17 +1,24 @@
 $(document).ready(function () {
-    $(window).scroll(function () {
-        if (window.pageYOffset > 1000 - $(window).height()) {
-            $('#labNav').removeClass('bg-semidark').addClass('bg-dark');
-            $('.top-button').show();
-            $('.go-top').click(function (e) {
-                window.scrollTo(0, 0)
-                $('.top-button').hide();
-            })
-        } else {
-            $('#labNav').addClass('bg-semidark').removeClass('bg-dark');
-        }
 
+    // Transition effect for navbar 
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 300) {
+            $('#labNav').addClass('bg-dark');
+            $('.top-button').show();
+
+        } else {
+            $('#labNav').removeClass('bg-dark');
+            $('.top-button').hide();
+
+        }
     });
+
+    $('.go-top').click(function (e) {
+        window.scrollTo(0, 0)
+        $('#labNav').addClass('bg-semidark').removeClass('bg-dark');
+        $('.top-button').hide();
+    })
 
     $('.toast').toast('show');
 
@@ -35,4 +42,39 @@ $(document).ready(function () {
             window.location.replace(currentUrl);
         }
     })
+
+    $('#v-pills-tab a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+    })
+
+    //Tabs function not to jump on click in User-profile.html
+    if (location.hash) {
+        $('a[href=\'' + location.hash + '\']').tab('show');
+    }
+    var activeTab = localStorage.getItem('activeTab');
+    if (activeTab) {
+        $('a[href="' + activeTab + '"]').tab('show');
+    }
+
+    $('body').on('click', 'a[data-toggle=\'pill\']', function (e) {
+        e.preventDefault()
+        var tab_name = this.getAttribute('href')
+        if (history.pushState) {
+            history.pushState(null, null, tab_name)
+        } else {
+            location.hash = tab_name
+        }
+        localStorage.setItem('activeTab', tab_name)
+
+        $(this).tab('show');
+        return false;
+    });
+    $(window).on('popstate', function () {
+        var anchor = location.hash ||
+            $('a[data-toggle=\'pill\']').first().attr('href');
+        $('a[href=\'' + anchor + '\']').tab('show');
+    });
+
+
 });
