628b55fd2327c537d5235f4a0aff7f9e61b310c7
john <jericcarreon@yahoo.com>
2020-09-26 02:43:37 +0100

    Added membership.views all membership and membership detail

--- membership/views.py
+++ membership/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
-from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-
-from django.db.models import Q
-from django.db.models.functions import Lower
+from django.utils.dateparse import parse_date
+from datetime import timedelta
+from django.utils import timezone
 
 from membership.models import Membership, UserMembership, Subscription
 
@@ -15,11 +14,35 @@ def all_membership(request):
     """ renders all membership"""
 
     membership = Membership.objects.all()
-    monthly = membership[0]
-    annually = membership[1]
     context = {
-        'monthly': monthly,
-        'annually': annually,
+        'membership': membership,
+
     }
 
     return render(request, 'membership.html', context)
+
+
+@login_required
+def membership_detail(request, membership_id):
+    """ Renders the product details """
+
+    membership = get_object_or_404(Membership, pk=membership_id)
+    subscription = Subscription.objects.all()
+
+    date = None
+    exp_date = 0
+    if request.method == 'POST':
+        date_str = request.POST.get('date')
+        date = parse_date(date_str)
+        exp_date = date + timedelta(days=membership.duration_days)
+
+    exp_date = exp_date
+
+    context = {
+        'membership': membership,
+        'subscription': subscription,
+        'date': date,
+        'exp_date': exp_date,
+    }
+
+    return render(request, 'membership_detail.html', context)
