bb79ee3eb911ea7c53e01e88b1465c8c8e0db5b1
john <jericcarreon@yahoo.com>
2020-09-27 22:53:44 +0100

    Added path for membership checkout and success

--- checkout/urls.py
+++ checkout/urls.py
@@ -5,5 +5,11 @@ from .webhooks import webhook
  """
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('membership/<int:membership_id>/', views.membership_checkout,
+         name='membership_checkout'),
     path('thanks/<order_number>', views.thanks, name='thanks'),
+    path('membership/payment/<int:membership_id>',
+         views.membership_success,
+         name='membership_success'),
+
 ]
