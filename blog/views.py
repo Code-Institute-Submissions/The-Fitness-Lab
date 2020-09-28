fc3169db28e50bf3d7456fefb10966bf4ae9f874
john <jericcarreon@yahoo.com>
2020-09-28 02:35:10 +0100

    Added Create_blog function

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import redirect, render, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 from .models import Post, PostComment
-from .forms import CreatePost, CommentPostForm
+from .forms import CreatePostForm, CommentPostForm
 
 from django.utils import timezone
 from datetime import datetime, timedelta
@@ -9,10 +9,31 @@ import datetime
 
 
 def blog(request):
-    post = Post.objects.all()
+    post = Post.objects.filter(published_date__lte=timezone.now()
+                               ).order_by('-published_date')
     template = 'blog.html'
     context = {
         'post': post,
     }
 
+    return render(request, template, context)
+
+
+@login_required
+def create_blog(request):
+    if request.method == 'POST':
+        form = CreatePostForm(request.POST, request.FILES)
+        post = Post.objects.all()
+        if form.is_valid():
+            instance = form.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            return redirect(reverse('blog'))
+    else:
+        form = CreatePostForm()
+
+    template = 'create_blog.html'
+    context = {
+        'form': form,
+    }
     return render(request, template, context)
