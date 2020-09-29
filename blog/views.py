2bf5c81ae6d37f4c28150c7a748e284d87e19b0c
john <jericcarreon@yahoo.com>
2020-09-30 00:16:37 +0100

    Added edit_blog_post function and added comments

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import redirect, render, get_object_or_404, reverse
 from django.contrib.auth.decorators import login_required
 from .models import Post, PostComment
-from .forms import CreatePostForm, PostCommentForm
-from django.contrib import messages
+from .forms import CreatePostForm, PostCommentForm, EditPostForm  # add before deployment
 from accounts.models import UserAccount
-from django.utils import timezone
-from datetime import datetime, timedelta
-import datetime
 
 
 def blog(request):
+    """
+    A view that allows us to view all the blog post
+    """
+
     blog = Post.objects.all()
     context = {
         'blog': blog,
@@ -20,29 +20,41 @@ def blog(request):
 
 @login_required
 def create_blog(request):
-    if request.method == 'POST':
-        form = CreatePostForm(request.POST, request.FILES)
-        blog = Post.objects.all()
-        if form.is_valid():
-            form.save()
-            return redirect('blog')
+    """
+    A view that allows us to create a blog post
+    """
 
-    else:
-        form = CreatePostForm()
+      profile = get_object_or_404(UserAccount, user=request.user)
+       if request.method == 'POST':
+            form = CreatePostForm(request.POST, request.FILES)
+            blog = Post.objects.all()
+            if form.is_valid():
+                instance = form.save(commit=False)
+                instance.creator = request.user
+                instance.save()
+                return render(request, 'blog.html', {'blog': blog})
 
-    template = 'create_blog.html'
-    context = {
-        'form': form,
-    }
-    return render(request, template, context)
+        else:
+            form = CreatePostForm()
+
+        template = 'create_blog.html'
+        context = {
+            'form': form,
+            'profile': profile,
+        }
+        return render(request, template, context)
 
 
 @login_required
 def blog_details(request, post_id):
+    """ 
+    A view that allows us to view a specific blog post 
+    """
 
+    edit = CreatePostForm(request.POST, request.FILES)
     blog = get_object_or_404(Post, pk=post_id)
     blog.save()
-    blog_user = blog.user
+    blog_user = blog.creator
     comments = blog.postcomment.all()
     if request.method == 'POST':
         form = PostCommentForm(request.POST)
@@ -55,6 +67,7 @@ def blog_details(request, post_id):
         form = PostCommentForm()
     profile = get_object_or_404(UserAccount, user=request.user)
     context = {
+        'edit': edit,
         'blog': blog,
         'comments': comments,
         'form': form,
@@ -65,26 +78,30 @@ def blog_details(request, post_id):
 
 
 @login_required
-def edit_blog_post(request, pk=None):
-    """ A view that allows us to add a comment to a post """
+def edit_blog_post(request, pk):
+    """ 
+    A view that allows us to edit a blog post 
+    """
 
-    post = get_object_or_404(Post, pk=pk)
+    blog = get_object_or_404(Post, id=pk)
 
     if request.method == 'POST':
-        edit_form = CreatePostForm(request.POST, request.FILES, instance=post)
+        blog = get_object_or_404(Post, id=pk)
+        edit_form = CreatePostForm(request.POST, request.FILES, instance=blog)
         if edit_form.is_valid():
-            edit_form.save()
-            messages.success(request,
-                             'You have successfully updated your Blog')
-            return redirect('blog_details', post.id)
+            instance = edit_form.save(commit=False)
+            instance.creator = request.user
+            instance.save()
+            return redirect('blog_details', blog.id)
 
         else:
-            messages.error(request, 'Sorry, you are unable to edit this post')
             return redirect('blog')
     else:
-        edit_form = CreatePostForm(request.POST, request.FILES, instance=post)
+        edit_form = CreatePostForm(request.POST, request.FILES, instance=blog)
+
+    template = 'edit_blog.html'
     context = {
         'form': edit_form,
-        'post': post,
+        'blog': blog,
     }
-    return redirect(request, 'edit_blog.html', context)
+    return render(request, template, context)
