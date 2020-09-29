89d3979d019d6263472e7606c8bbb9830b51064c
john <jericcarreon@yahoo.com>
2020-09-29 12:04:23 +0100

    Addded url for path for blog_details and edit_blog_post

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.blog, name='blog'),
     path('create/', views.create_blog, name='create_blog'),
+    path('<int:post_id>/', views.blog_details, name='blog_details'),
+    path('edit/<int:pk>/',
+         views.edit_blog_post, name='edit_blog_post'),
 ]
